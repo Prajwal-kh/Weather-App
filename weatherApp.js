a602f989e5ae0738c4c6c0540fa6361fdcee2eb8
PrajwalKharode <60292269+Prajwal-kh@users.noreply.github.com>
2022-02-26 15:14:52 +0530

    change localhost to dynamic port.

--- weatherApp.js
+++ weatherApp.js
@@ -49,6 +49,6 @@ app.post("/weather",function(req,res){
   //res.send("Today's weather for "+cityName+" is ");
 });
 
-app.listen(3000,function(){
+app.listen(process.env.PORT||3000,function(){
   console.log("Connected to the local server 3000");
 });
